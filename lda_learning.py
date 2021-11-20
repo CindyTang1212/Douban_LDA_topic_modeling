536be27d4e1c22e987fcc02fda40af4512b477c4
CindyTang12 <txdgz0624@gmail.com>
2021-11-21 00:27:31 +0800

    增加了困惑度计算

--- lda_learning.py
+++ lda_learning.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/11/18 4:29 PM
+# @Time    : 2021/11/16 4:29 PM
 # @Author  : Cindy Tang
 # @FileName: lda_learning.py
 # @Software: PyCharm
@@ -12,31 +12,32 @@ import os
 import pyLDAvis
 import pyLDAvis.sklearn
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-# 预处理
-dicts_path = 'dicts/dictionaries'
-stop_words_path = "dicts/stopwords-master"
-# 加载用户字典
-my_words = ['九九六', '公务员', '内卷', '2020', '955', '找工作', '考编', '三十五岁', '宇宙尽头', '找不到工作',
-            '秋招', '考公', '体制内', '稳定', '躺平', '985废物',
-            '小镇做题家', '福利待遇', '同辈压力',
-            '一线城市', '二线城市', '十八线城市', '找工作', '应届生', '组员朋友', '事业单位']
-for i in my_words:
-    jieba.add_word(i)
-dicts_files = os.listdir(dicts_path)
-for file in dicts_files:
-    jieba.load_userdict(dicts_path + '/' + file)
-print('字典添加完成！')
-# 加载停用词
-stop_words = []
-stop_words_files = os.listdir(stop_words_path)
-for file in stop_words_files:
-    with open(stop_words_path + '/' + file, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        for line in lines:
-            stop_words.append(line.strip())
-print('停用词添加完成！')
+# # 预处理
+# dicts_path = 'dicts/dictionaries'
+# stop_words_path = "dicts/stopwords-master"
+# # 加载用户字典
+# my_words = ['九九六', '公务员', '内卷', '2020', '955', '找工作', '考编', '三十五岁', '宇宙尽头', '找不到工作',
+#             '秋招', '考公', '体制内', '稳定', '躺平', '985废物',
+#             '小镇做题家', '福利待遇', '同辈压力',
+#             '一线城市', '二线城市', '十八线城市', '找工作', '应届生', '组员朋友', '事业单位']
+# for i in my_words:
+#     jieba.add_word(i)
+# dicts_files = os.listdir(dicts_path)
+# for file in dicts_files:
+#     jieba.load_userdict(dicts_path + '/' + file)
+# print('字典添加完成！')
+# # 加载停用词
+# stop_words = []
+# stop_words_files = os.listdir(stop_words_path)
+# for file in stop_words_files:
+#     with open(stop_words_path + '/' + file, 'r', encoding='utf-8') as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             stop_words.append(line.strip())
+# print('停用词添加完成！')
 
 
 # 分词处理
@@ -68,6 +69,31 @@ def lda_learning(n_features, n_topics, contents):
     return tf, tf_vectorizer, lda, docres
 
 
+def get_perplexity(n_features, n_max_topics, contents):
+    # 建立lda模型，计算模型困惑度
+    plexs = []
+    tf_vectorizer = CountVectorizer(strip_accents='unicode',
+                                    max_features=n_features,
+                                    stop_words='english',
+                                    max_df=0.5,
+                                    min_df=10)
+    tf = tf_vectorizer.fit_transform(contents)
+    for i in range(1, n_max_topics + 1):
+        print(i, end=' : ')
+        lda = LatentDirichletAllocation(n_components=i, max_iter=50,
+                                        learning_method='online',
+                                        learning_offset=50, random_state=0)
+        lda.fit(tf)
+        print(lda.perplexity(tf), end='\n')
+        plexs.append(lda.perplexity(tf))
+    n_t = n_max_topics  # 区间最右侧的值。注意：不能大于n_max_topics
+    x = list(range(1, n_max_topics+1))
+    plt.plot(x, plexs[0:])
+    plt.xlabel("number of topics")
+    plt.ylabel("perplexity")
+    plt.show()
+
+
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         print("Topic #%d:" % topic_idx)
@@ -84,18 +110,23 @@ def doc_category(docres):
 
 
 if __name__ == '__main__':
-    # 分词处理
-    df = pd.read_csv('post_content/all.csv', error_bad_lines=False)
-    df['content_cutted'] = df['页面标题'].apply(my_cut) + df['文本'].apply(my_cut) + df['高赞评论1'].apply(my_cut) + df[
-        '高赞评论2'].apply(my_cut)
-    content = df['content_cutted']
-    content.to_csv('content_cutted.csv')
-    print('切割完成！')
+    # # 分词处理
+    # df = pd.read_csv('post_content/all.csv', error_bad_lines=False)
+    # df['content_cutted'] = df['页面标题'].apply(my_cut) + df['文本'].apply(my_cut) + df['高赞评论1'].apply(my_cut) + df[
+    #     '高赞评论2'].apply(my_cut)
+    # content = df['content_cutted']
+    # content.to_csv('content_cutted.csv')
+    # print('切割完成！')
 
-    # 建立lda模型
+    # # 困惑度计算
+    # df = pd.read_csv('content_cutted.csv', error_bad_lines=False)
+    # get_perplexity(n_features=1500, n_max_topics=10, contents=df['content_cutted'].values.astype('U'))
+
+    # 建立主题模型
     n_topics = 5
+    n_features = 1500
     df = pd.read_csv('content_cutted.csv', error_bad_lines=False)
-    tf, tf_vectorizer, lda, docres = lda_learning(n_features=1500, n_topics=n_topics,
+    tf, tf_vectorizer, lda, docres = lda_learning(n_features=n_features, n_topics=n_topics,
                                                   contents=df['content_cutted'].values.astype('U'))
     tf_feature_names = tf_vectorizer.get_feature_names()
 
@@ -104,4 +135,4 @@ if __name__ == '__main__':
 
     # 获取可视化结果
     pic = pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
-    pyLDAvis.save_html(pic, 'result/' + 'lda_pass' + str(n_topics) + '.html')
+    pyLDAvis.save_html(pic, 'result/' + 'lda_pass' + str(n_topics) + '_' + str(n_features) + '.html')
